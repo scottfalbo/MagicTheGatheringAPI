212d7494d47d3feacbf98bd7208cc1ec52a9863b
Scott Falbo <scottfalboart@gmail.com>
2024-04-10 23:23:42 -0700

    installed mtg api package, rolled back to net 6

--- MagicTheGatheringAPI/Program.cs
+++ MagicTheGatheringAPI/Program.cs
@@ -1,4 +1,10 @@
+using MtgApiManager.Lib.Service;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var serviceProvider = new MtgServiceProvider();
+var service = serviceProvider.GetCardService();
+builder.Services.AddSingleton(service);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
